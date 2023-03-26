207c64139689aecc32987475140bd6d4fecafccd
irailtonreis <irailton.oliveira91@gmail.com>
2023-03-26 11:25:02 -0300

    feat: altera visibilidade método

--- app/views/View.ts
+++ app/views/View.ts
@@ -9,7 +9,7 @@ export abstract class View<T> {
         console.log(template)
         this.elemento.innerHTML = template
     }
-    
-    abstract template(model: T): string 
+
+   protected abstract template(model: T): string 
 }
   

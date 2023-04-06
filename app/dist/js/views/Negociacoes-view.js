6520ffa8971df9133682463a5b020d32b139957d
irailtonreis <irailton.oliveira91@gmail.com>
2023-04-06 08:52:33 -0300

    refactor: Reorganiza o projeto para próximo módulo

--- app/dist/js/views/Negociacoes-view.js
+++ app/dist/js/views/Negociacoes-view.js
@@ -1,9 +1,6 @@
-import { Negociacao } from "../models/Negociacao.js";
-import { Negociacoes } from "../models/Negociacoes.js";
 import { View } from "./View.js";
-
-export class NegociacoesView extends View<Negociacoes> {
-    template(model: Negociacoes): string {
+export class NegociacoesView extends View {
+    template(model) {
         return `
             <table class="table table-hover table-bordered"> 
                 <thead>
@@ -28,13 +25,12 @@ export class NegociacoesView extends View<Negociacoes> {
             </table>
         `;
     }
-
-    upadate(model: Negociacoes): void {
+    upadate(model) {
         const template = this.template(model);
-        console.log(template)
-        this.elemento.innerHTML = template
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
-    private formatar(data: Date): string {
-        return new Intl.DateTimeFormat().format(data)
+    formatar(data) {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
---
+++ app/dist/js/views/View.js
@@ -0,0 +1,23 @@
+export class View {
+    constructor(seletor, escapar) {
+        this.escapar = false;
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento;
+        }
+        else {
+            throw Error(`O Seletor ${seletor} não existe no DOM.`);
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+    upadate(model) {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<script>/, '');
+        }
+        console.log(template);
+        this.elemento.innerHTML = template;
+    }
+}
